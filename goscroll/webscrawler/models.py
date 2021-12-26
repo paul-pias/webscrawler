d0ef948d33fc28f164dfb08fb3cc9f7a99bb2792
paul-pias <paul.pias@northsouth.edu>
2021-12-27 01:05:52 +0600

    Redesigned Database tables

--- goscroll/webscrawler/models.py
+++ goscroll/webscrawler/models.py
@@ -1,3 +1,4 @@
+# from typing_extensions import Required
 from django.db import models
 from django.conf import settings
 
@@ -8,19 +9,34 @@ from django.dispatch import receiver
 # * title
 # * GUID
 # * Company
-# * Categories (multiple)
+# * Categories (multiple) ['python', 'scala', 'reactjs] 
 # * description
 # * Timestamps
 
+class Category(models.Model):
+    """
+        Make a  seperate table for types of jobs
+    """
+
+    name = models.CharField(max_length= 30)
+
+    def __str__(self):
+        return self.name
+
+
 class Data(models.Model):
+    """
+        Database columns for job query. 
+    """
 
     title = models.CharField(max_length = 300)
-    guid = models.IntegerField()
+    guid = models.IntegerField(primary_key = True)
     company = models.CharField(max_length = 50)
-    category = models.JSONField()
+    # category = models.CharField(max_length = 50)
+    categories = models.ManyToManyField(Category, related_name= 'datas', blank= True) 
     description = models.CharField(max_length = 5000)
-    pubDate = models.CharField(max_length = 50)
+    pubDate = models.CharField(max_length = 50)  ## DateTimeParser
     updateDate = models.CharField(max_length = 50)
 
     def __str__(self):
-        return self.guid
+        return str(self.guid)
