54bdd79656297c1ab78b7daba9dc6cb7bff7bfc2
paul-pias <paul.pias@northsouth.edu>
2021-12-30 02:56:11 +0600

    Adding files for utils

--- goscroll/utils/scrawler.py
+++ goscroll/utils/scrawler.py
@@ -1,7 +1,12 @@
 import csv, requests
-import xml.etree.ElementTree as ET
 import urllib.request
 from bs4 import BeautifulSoup
+from dateutil import parser
+from dateutil import tz
+import dateutil
+
+
+
 
 #https://stackoverflow.com/jobs/feed
 # * title
@@ -12,6 +17,16 @@ from bs4 import BeautifulSoup
 # * Timestamps
 
 class WebScrapper:
+    """
+        Takes url as input return a dictionary with appropriate key and values
+        Parameters:
+
+            urls (string) = A url as string
+
+        Returns:
+                A dictionary 
+
+        """
 
     def __init__(self, url, *args):
 
@@ -19,55 +34,37 @@ class WebScrapper:
         self.info = {}
         
     def get_data(self):
-
         # url = "https://stackoverflow.com/jobs/feed"
-        url = self.url
         response = requests.get(self.url)
         soup= BeautifulSoup(response.content,"lxml-xml")
-        itmes = soup.find_all('item')
+        items = soup.find_all('item')
         
+        for item in items:
+            guid = list(item.find_all('guid'))[0].get_text()                                            #GUID
+            
+            title = list(item.find_all('title'))[0].get_text()                                          #Title
+            
+            company_name = list(item.find_all('name'))[0].get_text()                                    #Company
+            
+            timezone_info = {"UT": dateutil.tz.UTC}
 
-        for item in itmes:
-            for id in item.find_all('guid'):
-                guid = id.get_text()                                            #GUID
-            for title in item.find_all('title'):
-                title = title.get_text()                                        #Title
-            for name in item.find_all('name'):
-                company_name = name.get_text()                                  #Company
-            for pub_time in item.find_all('pubDate'):
-                pubDate = pub_time.get_text()
-            for updated_time in item.find_all('updated'):
-                updated = updated_time.get_text()
+            pubDate = parser.parse(list(item.find_all('pubDate'))[0].get_text(), tzinfos=timezone_info)
+                                                                                                        #Timestamp
+            updated = parser.parse(list(item.find_all('updated'))[0].get_text(), tzinfos=timezone_info)
                 
-            time_stamp = {"pubDate":pubDate, "updateDate":updated}                                #TimeStamps
-
             categories = []
             for category in item.find_all('category'):
-                categories.append(category.get_text())                            #Categories
+                categories.append(category.get_text())                                                  #Categories
             
-            for description in soup.find_all('description'):
-                description = description.get_text()
+
+            for description in item.find_all('description'):
+                description = description.get_text()                                                    #Description
+            
             self.info.update({guid: {"title":title, 
                                         "company":company_name, 
-                                        "category":categories,
+                                        "categories":categories,
                                         "description":description, 
                                         "pubDate":pubDate,
-                                        "updateDate":updated}})
+                                        "updated":updated}})
         
         return self.info
-            # print("Self-info", self.info)
-# print(info)
-
-
-
-
-# print("Title = {} , GUID = {} , Companies = {} , Description = {}".format(len(titles), len(guids), len(companies), len(descriptions)))
-# for description in soup.find_all('description'):
-#     #print(list(description.children))
-#     #print(description.get_text())
-#     [print(type(item)) for item in list(description.children)]
-
-# for title, description in soup.find_all('title'), soup.find_all('description'):
-#     print("{} || {}".format(title.get_text(), description.get_text()))
-    # print("list-format {} == unknown-format {}".format((list(title)),title))
-    #print(soup.find_all('description'))
